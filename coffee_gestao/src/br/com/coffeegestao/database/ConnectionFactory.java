87da13cabc5fa6218d89f0c2e21c73fb6c360581
Pedro Aurélio <pedroaurelio.reis@gmail.com>
2026-07-02 16:16:24 -0300

    feat: add SQLite connection factory

--- coffee_gestao/src/br/com/coffeegestao/database/ConnectionFactory.java
+++ coffee_gestao/src/br/com/coffeegestao/database/ConnectionFactory.java
@@ -1,4 +1,18 @@
 package br.com.coffeegestao.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
+
+    public static final String URL = "jdbc:sqlite:coffeegestao.db";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao conectar com o banco de dados.", e);
+        }
+    }
 }
