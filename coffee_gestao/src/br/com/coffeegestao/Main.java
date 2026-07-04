edee3101cc9a4bec704b6bf8d5b6423d8f93a72b
Pedro Aurélio <pedroaurelio.reis@gmail.com>
2026-07-03 23:43:07 -0300

    feat: initialize database on app startup

--- coffee_gestao/src/br/com/coffeegestao/Main.java
+++ coffee_gestao/src/br/com/coffeegestao/Main.java
@@ -1,7 +1,12 @@
 package br.com.coffeegestao;
 
+import br.com.coffeegestao.database.DatabaseInitializer;
+
 public class Main {
     public static void main(String[] args) {
+        DatabaseInitializer.initialize();
+
+        System.out.println("Coffee Gestão iniciadod");
 
     }
 }
