abac1070f5693d7e2fb09f6ba9e0912342b01867
Pedro Aurélio <pedroaurelio.reis@gmail.com>
2026-07-04 16:45:14 -0300

    feat: add user model

--- src/main/java/br/com/coffeegestao/model/Usuario.java
+++ src/main/java/br/com/coffeegestao/model/Usuario.java
@@ -1,4 +1,90 @@
 package br.com.coffeegestao.model;
 
 public class Usuario {
+
+    private int id;
+    private String nome;
+    private String email;
+    private String senha;
+    private String perfil;
+    private boolean ativo;
+
+    public Usuario() {
+    }
+
+    public Usuario(int id, String nome, String email, String senha, String perfil, boolean ativo) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.perfil = perfil;
+        this.ativo = ativo;
+    }
+
+    public Usuario(String nome, String email, String senha, String perfil, boolean ativo) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.perfil = perfil;
+        this.ativo = ativo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public String getPerfil() {
+        return perfil;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void setPerfil(String perfil) {
+        this.perfil = perfil;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", perfil='" + perfil + '\'' +
+                ", ativo=" + ativo +
+                '}';
+    }
 }
