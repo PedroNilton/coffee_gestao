2ecc0e984cc6597301f2cbdb0945ccf892ea4421
Pedro Aurélio <pedroaurelio.reis@gmail.com>
2026-08-03 15:18:20 -0300

    feat: implement Cliente model

--- src/main/java/br/com/coffeegestao/model/Cliente.java
+++ src/main/java/br/com/coffeegestao/model/Cliente.java
@@ -1,4 +1,91 @@
 package br.com.coffeegestao.model;
 
 public class Cliente {
+
+    private int id;
+    private String nome;
+    private String telefone;
+    private String cpf;
+    private String email;
+    private String endereco;
+
+    public Cliente() {
+    }
+
+    public Cliente(int id, String nome, String telefone, String cpf, String email, String endereco) {
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.cpf = cpf;
+        this.email = email;
+        this.endereco = endereco;
+    }
+
+    public Cliente(String nome, String telefone, String cpf, String email, String endereco) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.cpf = cpf;
+        this.email = email;
+        this.endereco = endereco;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", email='" + email + '\'' +
+                ", endereco='" + endereco + '\'' +
+                '}';
+    }
 }
