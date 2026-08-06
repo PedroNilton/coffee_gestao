cbe98f0298a75f0af138d4bb86c6497ad0040aed
Pedro Aurélio <pedroaurelio.reis@gmail.com>
2026-08-06 19:08:54 -0300

    feat: add LoginController

--- src/main/java/br/com/coffeegestao/controller/LoginController.java
+++ src/main/java/br/com/coffeegestao/controller/LoginController.java
@@ -1,4 +1,18 @@
 package br.com.coffeegestao.controller;
 
+import br.com.coffeegestao.model.Usuario;
+import br.com.coffeegestao.repository.UsuarioRepository;
+import br.com.coffeegestao.service.AuthService;
+
 public class LoginController {
+
+    private final AuthService authService;
+
+    public LoginController() {
+        this.authService = new AuthService(new UsuarioRepository());
+    }
+
+    public Usuario login(String email, String senha) {
+        return authService.autenticar(email, senha);
+    }
 }
