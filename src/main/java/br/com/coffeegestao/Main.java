305048980000662f15b118b131a959c27fc5dfd5
Pedro Aurélio <pedroaurelio.reis@gmail.com>
2026-08-07 19:36:14 -0300

    feat: launch LoginView from Main on startup

--- src/main/java/br/com/coffeegestao/Main.java
+++ src/main/java/br/com/coffeegestao/Main.java
@@ -1,12 +1,14 @@
 package br.com.coffeegestao;
 
 import br.com.coffeegestao.database.DatabaseInitializer;
+import br.com.coffeegestao.view.LoginView;
+
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
         DatabaseInitializer.initialize();
 
-        System.out.println("Coffee Gestão iniciado.");
-
+        SwingUtilities.invokeLater(() -> new LoginView().setVisible(true));
     }
 }
