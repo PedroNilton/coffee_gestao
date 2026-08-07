ae0dfdc829850ebac4db8a6462eaab1af19debdb
Pedro Aurélio <pedroaurelio.reis@gmail.com>
2026-08-07 19:51:25 -0300

    feat: implement Produto model

--- src/main/java/br/com/coffeegestao/model/Produto.java
+++ src/main/java/br/com/coffeegestao/model/Produto.java
@@ -1,4 +1,78 @@
 package br.com.coffeegestao.model;
 
 public class Produto {
+
+    private int id;
+    private String nome;
+    private String descricao;
+    private double preco;
+    private int quantidadeEstoque;
+
+    public Produto() {
+    }
+
+    public Produto(int id, String nome, String descricao, double preco, int quantidadeEstoque) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public Produto(String nome, String descricao, double preco, int quantidadeEstoque) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public void setQuantidadeEstoque(int quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", preco=" + preco +
+                ", quantidadeEstoque=" + quantidadeEstoque +
+                '}';
+    }
 }
